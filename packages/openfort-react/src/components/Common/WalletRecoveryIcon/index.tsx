c066189c116ef041a3acb3e61bc28ee656d47301
Antoni Galí <antonigaligimeno@gmail.com>
2026-03-25 18:10:10 +0100

    feat:improved wallet recover page and wallet chosen

--- packages/openfort-react/src/components/Common/WalletRecoveryIcon/index.tsx
+++ packages/openfort-react/src/components/Common/WalletRecoveryIcon/index.tsx
@@ -1,6 +1,12 @@
 import { RecoveryMethod } from '@openfort/openfort-js'
 import { FingerPrintIcon, KeyIcon, LockIcon } from '../../../assets/icons'
 
+export const RECOVERY_METHOD_LABEL: Record<RecoveryMethod, string> = {
+  [RecoveryMethod.AUTOMATIC]: 'Automatic',
+  [RecoveryMethod.PASSWORD]: 'Password',
+  [RecoveryMethod.PASSKEY]: 'Passkey',
+}
+
 export const WalletRecoveryIcon = ({ recovery }: { recovery: RecoveryMethod | undefined }) => {
   switch (recovery) {
     case RecoveryMethod.PASSWORD:
